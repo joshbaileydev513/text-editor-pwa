65ea8fb7b0b7f92a4e2e741def17eb99d9737626
joshbaileydev513 <joshbaileydev513@gmail.com>
2023-06-14 15:00:03 -0400

    added/configured plugins for SW and manifest file. Also added CSS loaders and babel to webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -2,9 +2,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// Added and configure workbox plugins for a service worker and manifest file.
 
 module.exports = () => {
   return {
@@ -18,12 +18,54 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
+      // need to put workbox plugin still but the manifest file is below
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text Editor App',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor',
+        background_color: '',
+        theme_color: '',
+        start_url: './',
+        public_path: './',
+        icons: [
+          {
+            // not entirely sure if I am going to need this or not yet but figured I would put it in here
+            // src: ,
+            // sizes: ,
+            // destination: ,
+          }
+        ]
+      }),
+      new WorkboxPlugin.GenerateSW(),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }), 
     ],
-
+// Added CSS loaders and babel to webpack.
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
